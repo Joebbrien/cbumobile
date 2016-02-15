f810157cbd0353f2ceabf30154188e818e7435ca
Joebbrien <job.bundabunda@gmail.com>
2016-02-15 14:54:39 +0200

    Update students.php

--- students.php
+++ students.php
@@ -2,4 +2,11 @@
  print("Trying to test collaborator push..");
  echo("I have added this");
  echo("We know how to use github");
+
+ function add($first, $second){
+  $sum= $first + $second;
+  
+  return $sum;
+ 
+ }
 ?>
